e9dd4313ad1fb6055bdfa85614d54c5a8e852631
Ederson Torresini <boidacarapreta@gmail.com>
2022-07-14 03:15:38 +0000

    Corrigir problema de falta de vídeo

--- mesa-de-controle.py
+++ mesa-de-controle.py
@@ -7,7 +7,8 @@ import pygame
 def main():
     load_dotenv()
 
-    # Definir as variáveis da porta serial
+    # Definir as variáveis de video e porta serial
+    getenv("SDL_VIDEODRIVER", default="dummy")
     serial_port = getenv("SERIAL_PORT", default="/dev/ttyACM0")
     serial_speed = getenv("SERIAL_SPEED", default=115200)
     try:
@@ -27,15 +28,15 @@ def main():
 
     # Mapear os botões e bairros
     botoes = {
-        1: 3, # Bairro 1 = botão 3 (Y)
-        2: 0, # Bairro 2 = botão 0 (A)
-        3: 2, # Bairro 3 = botão 2 (X)
-        4: 1, # Bairro 4 = botão 1 (B)
-        5: 4, # Bairro 5 = botão 4 (LB)
-        6: 5, # Bairro 6 = botão 5 (RB)
-        7: 6, # Bairro 7 = botão 6 (Back)
-        8: 7, # Bairro 8 = botão 7 (Start)
-        9: 8, # Bairro 9 = botão 8 (Xbox)
+        1: 3,  # Bairro 1 = botão 3 (Y)
+        2: 0,  # Bairro 2 = botão 0 (A)
+        3: 2,  # Bairro 3 = botão 2 (X)
+        4: 1,  # Bairro 4 = botão 1 (B)
+        5: 4,  # Bairro 5 = botão 4 (LB)
+        6: 5,  # Bairro 6 = botão 5 (RB)
+        7: 6,  # Bairro 7 = botão 6 (Back)
+        8: 7,  # Bairro 8 = botão 7 (Start)
+        9: 8   # Bairro 9 = botão 8 (Xbox)
     }
 
     # Iniciar os estados dos bairros
